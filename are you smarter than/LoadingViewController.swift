1b172e69f71aa39041b73fa81dac9fa532600d9e
Overtime <Truen2005@gmail.com>
2024-10-28 15:49:01 -0400

    feat: Add dynamic layout with container view for activity indicator

--- are you smarter than/LoadingViewController.swift
+++ are you smarter than/LoadingViewController.swift
@@ -10,14 +10,27 @@ class LoadingViewController: UIViewController {
     private func setupUI() {
         view.backgroundColor = .white
 
+        // Create an activity indicator
         let activityIndicator = UIActivityIndicatorView(style: .large)
         activityIndicator.translatesAutoresizingMaskIntoConstraints = false
         activityIndicator.startAnimating()
         view.addSubview(activityIndicator)
 
+        // Add a container view to ensure dynamic layout adapts to device screen
+        let containerView = UIView()
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(containerView)
+
         NSLayoutConstraint.activate([
-            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            // Center the container view in the main view
+            containerView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            containerView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            containerView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9),
+            containerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.2),
+
+            // Center the activity indicator within the container view
+            activityIndicator.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+            activityIndicator.centerYAnchor.constraint(equalTo: containerView.centerYAnchor)
         ])
     }
 }
